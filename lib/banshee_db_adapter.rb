dfc0140418d4deb19ff553d087012bbc939f6849
Jhoan <jhoanegar@gmail.com>
2014-01-21 18:54:20 -0600

    Add read_songs to banshee adapter

--- lib/banshee_db_adapter.rb
+++ lib/banshee_db_adapter.rb
@@ -1,4 +1,18 @@
 require 'sqlite3'
-
+require 'song'
 class BansheeDBAdapter
+  SELECT_QUERY = <<-END
+    SELECT TrackID, Uri, PlayCount 
+    FROM CoreTracks
+    WHERE PlayCount > 0
+    END
+
+  def read_songs(file)
+    songs = []
+    library = SQLite3::Database.new(file)
+    library.execute(SELECT_QUERY).each do |row|
+      songs.push(Song.new key:row[0].to_s, uri:row[1], play_count:row[2])
+    end
+    songs
+  end
 end
