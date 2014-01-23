0ed297ed0a82473dea4cfe57ddad0f53b24bcda0
Jhoan <jhoanegar@gmail.com>
2014-01-23 15:08:08 -0600

    Refactor bakcup guy tests

--- spec/backup_guy_spec.rb
+++ spec/backup_guy_spec.rb
@@ -12,31 +12,56 @@ describe BackupGuy, fakefs:true  do
   FILENAME = "file.db"
   TEST_FILE = HOME_DIR + "directory/#{FILENAME}"
 
-  context 'when the files exist' do
-    before(:each) do
-      FileUtils.mkpath(HOME_DIR + "directory")
-      FileUtils.touch(TEST_FILE)
-    end
-
-    context 'when no options are given' do
-      after(:each)  { `rm #{DEFAULT_BACKUP_DIR} -Rf ` }
-
-      it 'creates the backup in the default directory' do
-        @backup_guy.backup(TEST_FILE)
-        expect(File.exists?(DEFAULT_BACKUP_DIR+FILENAME)).to be_true
-      end
-    end
-
-    context 'when a custom backup dir is given' do
+  context 'when backing up files' do
+    
+    context 'with one file' do 
       before(:each) do
-        @backup_guy = BackupGuy.new backup_dir: CUSTOM_BACKUP_DIR 
+        FileUtils.mkpath(HOME_DIR + "directory")
+        FileUtils.touch(TEST_FILE)
       end
 
-      it 'creates the backup in the custom directory' do
-        @backup_guy.backup(TEST_FILE)
-        expect(File.exists?(CUSTOM_BACKUP_DIR+FILENAME)).to be_true
+      context 'when no options are given' do
+        after(:each)  { `rm #{DEFAULT_BACKUP_DIR} -Rf ` }
+
+        it 'creates the backup in the default directory' do
+          @backup_guy.backup(TEST_FILE)
+          expect(File.exists?(DEFAULT_BACKUP_DIR+FILENAME)).to be_true
+        end
+      end
+
+      context 'when a custom backup dir is given' do
+        before(:each) do
+          @backup_guy = BackupGuy.new backup_dir: CUSTOM_BACKUP_DIR 
+        end
+
+        it 'creates the backup in the custom directory' do
+          @backup_guy.backup(TEST_FILE)
+          expect(File.exists?(CUSTOM_BACKUP_DIR+FILENAME)).to be_true
+        end
       end
     end
 
+    context 'with several files' do
+      before(:each) do
+        @files = []
+        3.times do |i| 
+          @files << "#{TEST_FILE}#{i}" 
+          FileUtils.mkpath(HOME_DIR + "directory")
+          FileUtils.touch(@files.last)
+        end
+      
+      end
+
+      it 'creates the backup' do
+        @backup_guy.backup(*@files)
+        3.times do |i|
+          file = "#{DEFAULT_BACKUP_DIR+FILENAME+i.to_s}"
+          expect(File.exists?(file)).to be_true 
+        end
+      end
+    end
   end
+
+  context "when restoring files" do
+    
 end
